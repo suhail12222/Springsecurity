c4a5fcc0b06695e457fd5a71c3f3f71c46276225
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-28 12:47:08 +0530

    adding new features

--- service/AuthService.java
+++ service/AuthService.java
@@ -1,6 +1,7 @@
 package com.practices.demo.service;
 
 import com.practices.demo.dto.LoginDto;
+import com.practices.demo.dto.LoginResponseDto;
 import com.practices.demo.entities.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
@@ -13,11 +14,20 @@ import org.springframework.security.core.Authentication;
 @RequiredArgsConstructor
 public class AuthService {
     private final JwtService jwtService;
+    private final UserService userService;
     private  final AuthenticationManager authenticationManager;
-    public  String login(LoginDto loginDto){
+    public LoginResponseDto login(LoginDto loginDto){
         Authentication authenticate=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getEmail(),loginDto.getPassword()));
         UserEntity user=(UserEntity)authenticate.getPrincipal();
-        String token=jwtService.generateSecretKey(user);
-        return token;
+        String acessToken=jwtService.generateSecretKey(user);
+        String refreshToken= jwtService.getRefreshToken(user);
+        return new LoginResponseDto(user.getId(), acessToken,refreshToken);
+    }
+
+    public LoginResponseDto refrehToken(String refreshToken) {
+        Long userId=jwtService.getValidate(refreshToken);
+        UserEntity user=userService.getUserByID(userId);
+        String acessToken=jwtService.generateSecretKey(user);
+        return new LoginResponseDto(user.getId(),acessToken,refreshToken);
     }
 }
