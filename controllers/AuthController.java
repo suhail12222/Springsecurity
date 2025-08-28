1ec421c46fb6b56bbaea298dd845c3a402d1bfca
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-28 12:44:30 +0530

    adding LoginResponseDto as Return and add refresh api to get the acess token with the lep of resfresh token

--- controllers/AuthController.java
+++ controllers/AuthController.java
@@ -1,16 +1,21 @@
 package com.practices.demo.controllers;
 
-import com.practices.demo.dto.LoginDto;
-import com.practices.demo.dto.PostDto;
-import com.practices.demo.dto.SignupDto;
-import com.practices.demo.dto.UserDto;
+import com.practices.demo.dto.*;
+import com.practices.demo.entities.UserEntity;
 import com.practices.demo.service.AuthService;
+import com.practices.demo.service.JwtService;
 import com.practices.demo.service.PostService;
 import com.practices.demo.service.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -19,15 +24,34 @@ import java.util.List;
 public class AuthController {
     private final UserService userService;
     private final AuthService authService;
+    private final JwtService jwtService;
 @PostMapping("/signup")
     public ResponseEntity<UserDto> signUp(@RequestBody SignupDto signupDto){
     UserDto userDto= userService.signUp(signupDto);
     return ResponseEntity.ok(userDto);
 } //   when user sigin we provide jwt token from here to maintain  for one min only next part will be to give refresh token after this token expiration.\\
 @PostMapping("/signin")
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
-    String token= authService.login(loginDto);
-    return ResponseEntity.ok(token);
+    public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto, HttpServletRequest req, HttpServletResponse res){
+    LoginResponseDto loginResponseDto=authService.login(loginDto);
+    Cookie cookie=new Cookie("refreshToken", loginResponseDto.getRefreshToken());
+    cookie.setHttpOnly(true);
+    res.addCookie(cookie);
+//    String token= authService.login(loginDto);
+    return ResponseEntity.ok(loginResponseDto);
 
+    }
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> getRefresh(HttpServletRequest req,HttpServletResponse res){
+      String refreshToken=  Arrays.stream(req.getCookies())
+                .filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(()->new AuthenticationServiceException("refreshToken not found inside the cookies"));
+
+   LoginResponseDto loginResponseDto=authService.refrehToken(refreshToken);
+
+
+
+   return  ResponseEntity.ok(loginResponseDto);
     }
 }
