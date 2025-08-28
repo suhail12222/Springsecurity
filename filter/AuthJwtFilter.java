5c755ac6bbc745bbeaa6112ceb26d43e151a3ebd
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-28 12:46:06 +0530

    add another method to get refresh token

--- filter/AuthJwtFilter.java
+++ filter/AuthJwtFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,6 +17,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
@@ -23,54 +26,58 @@ import java.io.IOException;
 public class AuthJwtFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserService userService;
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain)
-            throws ServletException, IOException {
+      {
+        try {
 
-        final String requestTokenHeader = request.getHeader("Authorization");
+            final String requestTokenHeader = request.getHeader("Authorization");
 
-        //  Skip if no header or does not start with "Bearer "
-        if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")) {
+            //  Skip if no header or does not start with "Bearer "
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer ")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            //  Extract token from "Bearer <token>"
+            String token = requestTokenHeader.substring(7);
+
+            //  Validate token and extract userId
+            Long userId = jwtService.getValidate(token);
+
+            //  If token valid and SecurityContext is empty
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserEntity user = userService.getUserByID(userId);
+
+                // If your UserEntity implements UserDetails, use user.getAuthorities()
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                user,
+                                null,
+                                user.getAuthorities() // important if you use roles
+                        );
+
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                //  Store authentication in SecurityContext
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+            //  Continue the filter chain
             filterChain.doFilter(request, response);
-            return;
+        }catch (Exception e){
+            handlerExceptionResolver.resolveException(request,response,null,e);
         }
 
-        //  Extract token from "Bearer <token>"
-        String token = requestTokenHeader.substring(7);
 
-        //  Validate token and extract userId
-        Long userId = jwtService.getValidate(token);
-
-        //  If token valid and SecurityContext is empty
-        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserEntity user = userService.getUserByID(userId);
-
-            // If your UserEntity implements UserDetails, use user.getAuthorities()
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(
-                            user,
-                            null,
-                            user.getAuthorities() // important if you use roles
-                    );
-
-            authenticationToken.setDetails(
-                    new WebAuthenticationDetailsSource().buildDetails(request)
-            );
-
-            //  Store authentication in SecurityContext
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        }
-
-        //  Continue the filter chain
-        filterChain.doFilter(request, response);
     }
-
-
-
-
-
 
 }
