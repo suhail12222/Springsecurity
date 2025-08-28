3dad0952d8b1a1e814711c530f5f849e16851582
suhail ahmed malik <muhmmadasifmalik593@gmail.com>
2025-08-28 12:42:56 +0530

    token realted exceptions and authentication exceptions handle globaly

--- advice/GlobalExceptionHandler.java
+++ advice/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.practices.demo.advice;
 
 import com.practices.demo.exception.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
 
 
 @RestControllerAdvice
@@ -15,5 +18,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError>handleResourceNotFoundException(ResourceNotFoundException ex){
         ApiError error=new ApiError(ex.getLocalizedMessage(),HttpStatus .NOT_FOUND);
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }//AccountExpierdException//BadCrediantials//CreadinatialsException//sessionException
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError>handleAuthenticationException(AuthenticationException ex){
+        ApiError apiError=new ApiError(ex.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException exc){
+        ApiError apiError=new ApiError(exc.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+
     }
 }
